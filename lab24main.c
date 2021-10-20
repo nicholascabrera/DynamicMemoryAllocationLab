e0a993f107624602bc4f251d7882687f0e8d86cf
treangra000 <c24grace.treanor@afacademy.af.edu>
2021-10-19 19:29:24 -0600

    new line

--- lab24main.c
+++ lab24main.c
@@ -9,11 +9,10 @@ int main(void){
     cadetRecords = (CadetInfoStructType*) malloc(numRecords * sizeof(CadetInfoStructType));
 
     getDataText(cadetRecords, numRecords, "lab24data.txt");
-     
     printf("Reading number of records in the data file.\n");
     printf("%d records in the data file.\n", numRecords);
-    printf("The first cadet is:\nCadet name = %s\nCadet age = %d\nCadet squad = %d\nCadet year = %d", cadetRecords[0].name, cadetRecords[0].age, cadetRecords[0].squad, cadetRecords[0].year);
-    printf("The last cadet is:\nCadet name = %s\nCadet age = %d\nCadet squad = %d\nCadet year = %d", cadetRecords[120].name, cadetRecords[120].age, cadetRecords[120].squad, cadetRecords[120].year);
+    printf("The first cadet is:\nCadet name = %s\nCadet age = %d\nCadet squad = %d\nCadet year = %d\n", cadetRecords[0].name, cadetRecords[0].age, cadetRecords[0].squad, cadetRecords[0].year);
+    printf("The last cadet is:\nCadet name = %s\nCadet age = %d\nCadet squad = %d\nCadet year = %d\n", cadetRecords[120].name, cadetRecords[120].age, cadetRecords[120].squad, cadetRecords[120].year);
     free(cadetRecords);
 
     return 0;
